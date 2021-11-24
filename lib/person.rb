8fb6fced28bc7f84fc8a0f97495c0e8280d8b188
Goldie <goldiejackson25@yahoo.com>
2021-11-24 12:42:12 -0600

    commit tree 041301c04cc6ba2b647ed292cc452524be377830

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,20 @@
-# your code goes here
+require 'pry'
+class Person
+    attr_accessor :bank_account
+    if :happiness < 10
+        attr_accessor :happiness
+    end
+    attr_reader :name
+    def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+    end
+
+    def name
+        @name
+    end
+
+
+end
